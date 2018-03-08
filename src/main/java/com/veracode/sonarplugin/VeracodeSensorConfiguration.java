2ad04ecee6209171bee9888da81fcd9a3e75daa5
Kevin Rise <krise@veracode.com>
2018-03-08 10:12:44 -0500

    Proxy support is working.

--- src/main/java/com/veracode/sonarplugin/VeracodeSensorConfiguration.java
+++ src/main/java/com/veracode/sonarplugin/VeracodeSensorConfiguration.java
@@ -20,6 +20,10 @@ public class VeracodeSensorConfiguration {
     //private static String m_veracodeScanType;
     private static String m_veracodeApiId;
     private static String m_veracodeApiKey;
+    private static String m_proxyHost;
+    private static String m_proxyPort;
+    private static String m_proxyUsername;
+    private static String m_proxyPassword;
 
     /** future planning 
     private static Boolean m_staticResult = false;
@@ -31,6 +35,11 @@ public class VeracodeSensorConfiguration {
     //private static final String VERACODE_SCAN_TYPE = "sonar.veracode.scanType";
     private static final String VERACODE_API_ID = "sonar.veracode.apiId";
     private static final String VERACODE_API_KEY = "sonar.veracode.apiKey";
+    private static final String VERACODE_PROXY_HOST = "sonar.veracode.proxyHost";
+    private static final String VERACODE_PROXY_PORT = "sonar.veracode.proxyPort";
+    private static final String VERACODE_PROXY_USERNAME = "sonar.veracode.proxyUsername";
+    private static final String VERACODE_PROXY_PASSWORD = "sonar.veracode.proxyPassword";
+
 
     public VeracodeSensorConfiguration(SensorContext context) {
 
@@ -43,10 +52,15 @@ public class VeracodeSensorConfiguration {
         m_veracodeApiId = context.config().get(VERACODE_API_ID).orElse(null);
         m_veracodeApiKey = context.config().get(VERACODE_API_KEY).orElse(null);
 
+        m_proxyHost = context.config().get(VERACODE_PROXY_HOST).orElse(null);
+        m_proxyPort = context.config().get(VERACODE_PROXY_PORT).orElse(null);
+        m_proxyUsername = context.config().get(VERACODE_PROXY_USERNAME).orElse(null);
+        m_proxyPassword = context.config().get(VERACODE_PROXY_PASSWORD).orElse(null);
+
         log.debug("Veracode app name = " + m_veracodeAppName);
         log.debug("Veracode API ID = " + m_veracodeApiId);
 
-        // TODO: encryption on the API key?  Or allow via command-line instead
+        // TODO: encryption on the API key?
 
     }
 
@@ -60,5 +74,21 @@ public class VeracodeSensorConfiguration {
 
     public String getApiKey() {
         return m_veracodeApiKey;
+    }
+
+    public String getProxyHost() {
+        return m_proxyHost;
+    }
+
+    public String getProxyPort() {
+        return m_proxyPort;
+    }
+
+    public String getProxyUsername() {
+        return m_proxyUsername;
+    }
+
+    public String getProxyPassword() {
+        return m_proxyPassword;
     }
 }
