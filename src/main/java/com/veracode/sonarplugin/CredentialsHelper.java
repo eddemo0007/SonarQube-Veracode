73635e942c423d7268bc2339ed3ba2a92326ca7b
Kevin Rise <krise@veracode.com>
2018-03-08 10:12:44 -0500

    Proxy support is working.

--- src/main/java/com/veracode/sonarplugin/CredentialsHelper.java
+++ src/main/java/com/veracode/sonarplugin/CredentialsHelper.java
@@ -1,5 +1,6 @@
 package com.veracode.sonarplugin;
 
+import org.apache.commons.lang3.StringUtils;
 import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
 
@@ -23,18 +24,51 @@ public class CredentialsHelper {
 	{
         log.debug("Setting up API creds");
         
-        try
-        {
+        try {
             wrapper.setUpApiCredentials(m_config.getApiId(), m_config.getApiKey());
         }
-        catch(IllegalArgumentException e)
-        {
+        catch(IllegalArgumentException e) {
             // this can get thrown if the API Key is bogus
             log.error("Problem setting up API credentials.  Exception: " + e.toString());
             
             return false;				
         }
-		
+        
+        log.debug("Proxy Host = [" + m_config.getProxyHost() + "]");
+        log.debug("Proxy Port = [" + m_config.getProxyPort() + "]");
+        log.debug("Proxy Username = [" + m_config.getProxyUsername() + "]");
+        log.debug("Proxy Password = [" + m_config.getProxyPassword() + "]");
+
+        // setup proxy, if required
+        if(StringUtils.isNotBlank(m_config.getProxyHost()) ) {
+
+            log.debug("Setting up proxy");
+
+            // validate params
+            if(StringUtils.isBlank(m_config.getProxyPort()) ) {
+                log.error("Proxy Host is set, but not proxy port.");
+                return false;
+            }
+
+            if(StringUtils.isNotBlank(m_config.getProxyUsername()) && StringUtils.isBlank(m_config.getProxyPassword()) ) {
+                log.error("Proxy username is set, but not proxy password.");
+                return false;
+            }
+
+            // config the proxy
+            if(StringUtils.isNotBlank(m_config.getProxyUsername()) ) {
+                log.debug("4-way proxy");
+                wrapper.setUpProxy(m_config.getProxyHost(),
+                                    m_config.getProxyPort(), 
+                                    m_config.getProxyUsername(),
+                                    m_config.getProxyPassword());
+            }
+            else {
+                log.debug("2-way proxy");
+                wrapper.setUpProxy(m_config.getProxyHost(), m_config.getProxyPort());
+            }
+        }
+
 		return true;
 	}
 }
